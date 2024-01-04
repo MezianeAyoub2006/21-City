e42d5bb19144ce4e0a455e79c42e8390c09e8935
unknown <your.name@domain.com>
2024-01-04 20:31:00 +0100

    test

--- scripts/load_json.py
+++ scripts/load_json.py
@@ -1,7 +1,7 @@
 import json
 
-DEX_PATH = "C:/Users/Karima Meziane/Desktop/Black Jack/pokemon_data/dex.json"
-SPAWN_PATH = "C:/Users/Karima Meziane/Desktop/Black Jack/pokemon_data/spawn.json"
+DEX_PATH = "pokemon_data/dex.json"
+SPAWN_PATH = "pokemon_data/spawn.json"
 
 def load_dex():
     with open(DEX_PATH, "r") as file:
